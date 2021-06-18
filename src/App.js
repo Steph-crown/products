7df62c95d8a0348beb592e46797ff2bdde59c826
Steph-crown <emmanuelstephen024@gmail.com>
2021-06-18 01:45:45 +0100

    Created header and filter components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 
 export default function App() {
     return (
-        <div>
+        <div className="body">
             <header>
                 <Header />
             </header>
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,28 +1,27 @@
-import React, {useEffect} from 'react';
-
+import React, { useEffect } from "react";
+import "./../css/Filter.css";
 
 export default function Filter() {
     useEffect(() => {
         console.log("eff");
         return () => {
             console.log("pop");
-        }
-    }, [])
+        };
+    }, []);
     return (
         <React.Fragment>
             <div className="left-md">
                 <section>
-                <h3>Filter</h3>
+                    <h3>Filter</h3>
                 </section>
 
-                <section></section>
 
                 <section>
                     <h3>Price Range</h3>
                     <div className="flex">
                         <p className="muted">Min</p>
                         <label className="input-field">
-                        $<input type="number" />
+                            $<input type="number" />
                         </label>
                     </div>
                     <button>Set Price</button>
@@ -32,16 +31,11 @@ export default function Filter() {
                     <h3>Rating</h3>
                     <div className="flex">
                         <input type="checkbox" name="" id="" />
-                        <div className="star-comp">
-                            above
-                        </div>
+                        <div className="star-comp">above</div>
                     </div>
                 </section>
             </div>
-            <div className="filter-sm">
-
-            </div>
+            <div className="filter-sm"></div>
         </React.Fragment>
-        
-    )
+    );
 }
