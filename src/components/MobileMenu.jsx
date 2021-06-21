fd7e1ce100786a62b19e3666c536f60eca2baac1
Steph Crown <emmanuelstephen024@gmail.com>
2021-06-21 13:03:11 +0100

    Corrected an error

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -6,13 +6,13 @@ export default function MobileMenu({toggleMenu}) {
         <div className="mobile-menu">
             <p onClick={()=>toggleMenu()}>&times;</p>
             <div>
-                <a href="#" className="selected">Products</a>
-                <a href="#">About</a>
-                <a href="#">Activities</a>
-                <a href="#">Blogs</a>
-                <a href="#">Cart</a>
-                <a href="#">Favourite</a>
-                <a href="#">Profile</a>
+                <a href="/" className="selected">Products</a>
+                <a href="/">About</a>
+                <a href="/">Activities</a>
+                <a href="/">Blogs</a>
+                <a href="/">Cart</a>
+                <a href="/">Favourite</a>
+                <a href="/">Profile</a>
             </div>
         </div>
     );
